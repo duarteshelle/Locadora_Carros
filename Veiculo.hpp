07b2eea4cb61fa85636c38cc18b04d6c4c191684
Chr1s-J <103655149+Chr1s-J@users.noreply.github.com>
2023-07-03 16:07:03 -0300

    Create Veiculo.hpp

---
+++ Veiculo.hpp
@@ -0,0 +1,25 @@
+#ifndef VEICULO_H
+#define VEICULO_H
+
+#include <iostream>
+#include <string>
+
+class Veiculo {
+
+public:
+  std::string marca;
+  std::string tipo;
+  std::string modelo;
+  std::string cor;
+  std::string placa;
+  int ano;
+  std::string status;
+  std::string Status() {
+    
+  }
+
+private:
+
+};
+
+#endif
