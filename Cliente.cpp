c7b63230a8b303aa72e57dee00251a0a61925a72
Guilherme Alvarenga Argueso <gui.argueso@gmail.com>
2023-07-04 17:29:01 -0300

    Retirada Historico, implementacao em cliente

--- Cliente.cpp
+++ Cliente.cpp
@@ -9,66 +9,82 @@ Cliente::Cliente(std::string nome, std::string dataNascimento, std::string cpf, 
 
 Cliente::~Cliente() {}
 
-std::string Cliente::getEndereco() const {
+std::string Cliente::getEndereco() const
+{
     return endereco;
 }
 
-void Cliente::setEndereco(const std::string& endereco) {
+void Cliente::setEndereco(const std::string &endereco)
+{
     this->endereco = endereco;
 }
 
-std::string Cliente::getTelefone() const {
+std::string Cliente::getTelefone() const
+{
     return telefone;
 }
 
-void Cliente::setTelefone(const std::string& telefone) {
+void Cliente::setTelefone(const std::string &telefone)
+{
     this->telefone = telefone;
 }
 
-std::string Cliente::getCarteiraMotorista() const {
+std::string Cliente::getCarteiraMotorista() const
+{
     return carteiraMotorista;
 }
 
-void Cliente::setCarteiraMotorista(const std::string& carteiraMotorista) {
+void Cliente::setCarteiraMotorista(const std::string &carteiraMotorista)
+{
     this->carteiraMotorista = carteiraMotorista;
 }
 
-std::string Cliente::getCarteiraPagamento() const {
+std::string Cliente::getCarteiraPagamento() const
+{
     return carteiraPagamento;
 }
 
-void Cliente::setCarteiraPagamento(const std::string& carteiraPagamento) {
+void Cliente::setCarteiraPagamento(const std::string &carteiraPagamento)
+{
     this->carteiraPagamento = carteiraPagamento;
 }
 
-std::string Cliente::getDataNascimento() const {
+std::string Cliente::getDataNascimento() const
+{
     return dataNascimento;
 }
 
-void Cliente::setDataNascimento(const std::string& dataNascimento) {
+void Cliente::setDataNascimento(const std::string &dataNascimento)
+{
     this->dataNascimento = dataNascimento;
 }
 
-std::string Cliente::getCPF() const {
+std::string Cliente::getCPF() const
+{
     return cpf;
 }
 
-void Cliente::setCPF(const std::string& cpf) {
+void Cliente::setCPF(const std::string &cpf)
+{
     this->cpf = cpf;
 }
 
-std::string Cliente::getEmail() const {
+std::string Cliente::getEmail() const
+{
     return email;
 }
 
-void Cliente::setEmail(const std::string& email) {
+void Cliente::setEmail(const std::string &email)
+{
     this->email = email;
 }
 
-//void Cliente::adicionarHistorico(const std::string& historico) {     // historico  ***provavelmente n funciona ainda***
-//    this->historico.push_back(historico);                            // historico  ***provavelmente n funciona ainda***
-//}                                                                    // historico  ***provavelmente n funciona ainda***
+void Cliente::adicionarHistoricoCliente(const std::string &historicoCliente)
+{
+    this->historico.push_back(historicoCliente);
+}
 
-//std::vector<std::string> Cliente::getHistorico() const {             // historico  ***provavelmente n funciona ainda***
-//    return historico;                                                // historico  ***provavelmente n funciona ainda***
-//}                                                                    // historico  ***provavelmente n funciona ainda***
+std::vector<std::string> Cliente::getHistoricoCliente() const
+{
+    return historicoCliente;
+}
--- Cliente.hpp
+++ Cliente.hpp
@@ -5,16 +5,17 @@
 #include <vector>
 #include "Conta.hpp"
 
-class Cliente : public Conta {
+class Cliente : public Conta
+{
 private:
-    std::string endereco;           // endereço do cliente
-    std::string telefone;           // telefone do cliente
-    std::string carteiraMotorista;  // cart. motorista
-    std::string carteiraPagamento;  // cart. pagamento
-    std::string dataNascimento;     // data de nasc
-    std::string cpf;                // cpf
-    std::string email;              // email
-    //std::vector<std::string> historico; //historico de locacao ***provavelmente n funciona ainda***
+    std::string endereco;                      // endereço do cliente
+    std::string telefone;                      // telefone do cliente
+    std::string carteiraMotorista;             // cart. motorista
+    std::string carteiraPagamento;             // cart. pagamento
+    std::string dataNascimento;                // data de nasc
+    std::string cpf;                           // cpf
+    std::string email;                         // email
+    std::vector<std::string> historicoCliente; // historico de locacao do cliente
 
 public:
     Cliente(std::string nome, std::string dataNascimento, std::string cpf, std::string email,
@@ -23,28 +24,28 @@ public:
     ~Cliente();
 
     std::string getEndereco() const;
-    void setEndereco(const std::string& endereco);
+    void setEndereco(const std::string &endereco);
 
     std::string getTelefone() const;
-    void setTelefone(const std::string& telefone);
+    void setTelefone(const std::string &telefone);
 
     std::string getCarteiraMotorista() const;
-    void setCarteiraMotorista(const std::string& carteiraMotorista);
+    void setCarteiraMotorista(const std::string &carteiraMotorista);
 
     std::string getCarteiraPagamento() const;
-    void setCarteiraPagamento(const std::string& carteiraPagamento);
+    void setCarteiraPagamento(const std::string &carteiraPagamento);
 
     std::string getDataNascimento() const;
-    void setDataNascimento(const std::string& dataNascimento);
+    void setDataNascimento(const std::string &dataNascimento);
 
     std::string getCPF() const;
-    void setCPF(const std::string& cpf);
+    void setCPF(const std::string &cpf);
 
     std::string getEmail() const;
-    void setEmail(const std::string& email);
+    void setEmail(const std::string &email);
 
-    //void adicionarHistorico(const std::string& historico);  set/get do historico ***provavelmente n funciona ainda***
-    //std::vector<std::string> getHistorico() const;           ***provavelmente n funciona ainda***
+    void adicionarHistoricoCliente(const std::string &historicoCliente);
+    std::vector<std::string> getHistoricoCliente() const;
 };
 
 #endif
