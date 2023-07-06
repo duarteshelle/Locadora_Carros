6f348ebb70aec05f9e7f41c0c696af6b3814729e
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-06 00:24:49 -0300

    Update Admin.hpp
    Removido o método gerarRelatorio()

--- Admin.hpp
+++ Admin.hpp
@@ -2,20 +2,24 @@
 #define ADMIN_H
 
 #include "Conta.hpp"
+#include "Veiculo.hpp"
+#include "Locadora.hpp"
 
 #include <iostream>
 #include <string>
 #include <vector>
 
-class Admin : public Conta
-{
+class Admin : public Conta {
 private:
-    /* data */
+    
 public:
     Admin();
     ~Admin();
 
-    void gerarRelatorio(const std::vector<Veiculo> &veiculos) {}
 };
+
+
+
+
 
 #endif
