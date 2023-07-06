70450393c892d02a7d5b479d7bbecf6c86fe4176
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-06 00:29:00 -0300

    Create Locadora.hpp

---
+++ Locadora.hpp
@@ -0,0 +1,28 @@
+#ifndef LOCADORA_H
+#define LOCADORA_H
+
+#include "Carro.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Locadora{
+private:
+   std::vector<Carro> carros; 
+public:
+   
+    void adicionarCarro(Carro carro) {}
+    void listarCarrosDisponiveis() { }
+    void alugarCarro(std::string modelo) {}
+    void devolverCarro(std::string modelo) {}
+
+    Locadora();
+    ~Locadora();
+
+};
+
+
+
+
+#endif
