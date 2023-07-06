ef7c14eb534d4c300b280604f522422acfe7b50c
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-06 00:29:29 -0300

    Create Locadora.cpp

---
+++ Locadora.cpp
@@ -0,0 +1,53 @@
+#include "Locadora.hpp"
+
+
+Locadora::Locadora(){
+    //carros = std::vector<Carro>();
+}
+
+
+void Locadora::adicionarCarro(Carro carro) {
+    carros.push_back(carro);
+}
+
+
+void Locadora::listarCarrosDisponiveis() {
+    std::cout << "Carros disponíveis para locação:" << std::endl;
+    for (const auto& carro : carros) {
+        if (carro.disponivel) {
+            std::cout << "Marca: " << carro.marca << ", Modelo: " 
+            << carro.modelo << ", Ano: " << carro.ano << std::endl;
+        }
+    }
+}
+
+
+void Locadora::alugarCarro(std::string modelo) {
+    for (auto& carro : carros) {
+        if (carro.modelo == modelo && carro.disponivel) {
+            carro.disponivel = false;
+            std::cout << "Carro " << carro.marca << " " 
+            << carro.modelo << " alugado com sucesso!" << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "Não foi possível encontrar um carro disponível com o modelo especificado." << std::endl;
+}
+
+void Locadora::devolverCarro(std::string modelo) {
+    for (auto& carro : carros) {
+        if (carro.modelo == modelo && !carro.disponivel) {
+        carro.disponivel = true;
+        std::cout << "Carro " << carro.marca << " " << carro.modelo 
+        << " devolvido com sucesso!" << std::endl;
+        return;
+        }
+    }
+
+    std::cout << "Não foi possível encontrar um carro alugado com o modelo especificado." << std::endl;
+}
+
+
+
+Locadora::~Locadora(){}
