8bbd52a823f3e04d3750d6beb01764bec045df8c
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-06 18:47:40 -0300

    Update Carro.cpp

--- Carro.cpp
+++ Carro.cpp
@@ -1,19 +1,17 @@
 #include "Carro.hpp"
 
-Carro::Carro(std::string modelo, std::string marca, int ano, int placa){
-    modelo = modelo;
-    marca = marca;
-    ano = ano;
-    placa = placa;
+Carro::Carro(std::string modelo, std::string marca, int ano, std::string placa, double valorDiaria){
+    this->modelo = modelo;
+    this->marca = marca;
+    this->ano = ano;
+    this->placa = placa;
+    this->valorDiaria = valorDiaria;
 
-    arCondicionado = true;
-    marchaAutomatica = true;
-    motorFlex = true;
+    //arCondicionado = true;
+    //marchaAutomatica = true;
+    //motorFlex = true;
     disponivel = true;
 }
 
 
 Carro::~Carro(){}
-
-
-
