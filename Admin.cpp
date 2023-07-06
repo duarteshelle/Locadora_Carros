f700ff64a51bc3953e5a550b9370b358e220ff19
duarteshelle <103266297+duarteshelle@users.noreply.github.com>
2023-07-06 00:25:39 -0300

    Update Admin.cpp
    mudança da modificação no .hpp

--- Admin.cpp
+++ Admin.cpp
@@ -1,17 +1,5 @@
 #include "Admin.hpp"
 
+Admin::Admin(){}
 Admin::~Admin(){}
 
-void Admin::gerarRelatorio(const std::vector<Veiculo>& veiculos){
-    std::cout << "Relatório de Veículos Disponíveis:\n";
-    std::cout << "---------------------------------\n";
-    for (const auto& veiculo : veiculos) {
-        if (veiculo.disponivel) {
-            std::cout << "Marca: " << veiculo.marca << std::endl;
-            std::cout << "Modelo: " << veiculo.modelo << std::endl;
-            std::cout << "Ano: " << veiculo.ano << std::endl;
-            std::cout << "Disponível: Sim\n";
-            std::cout << "---------------------------------\n";
-        }
-    }
-}
